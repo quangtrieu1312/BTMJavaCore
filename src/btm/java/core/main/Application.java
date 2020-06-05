5abab3929e16b07b83be45addac9f15e4f100e12
Trieu Truong <quangtrieu1312@gmail.com>
2020-06-05 16:05:31 +0700

    add inner class(thread) & multithreading

--- src/btm/java/core/main/Application.java
+++ src/btm/java/core/main/Application.java
@@ -26,22 +26,106 @@ import btm.java.core.util.Validator;
 
 public class Application {
 
-	class LineSolver implements Runnable {
+	private static class LineSolver implements Runnable {
+		private Thread thread;
+
 		private String name;
-		private List<String> lines;
+		private Vector<String> lines;
 		private Integer startAtIndex;
 		private Integer endAtIndex;
+		private Vector<IEmployee> employees;
+		private Vector<String> errorLines;
+		private Vector<String> successLines;
 
-		public LineSolver(String name, List<String> lines, Integer startAtIndex, Integer endAtIndex) {
+		public LineSolver(String name, Vector<String> lines, Integer startAtIndex, Integer endAtIndex,
+				Vector<IEmployee> employees, Vector<String> errorLines, Vector<String> successLines) {
 			this.setName(name);
 			this.setLines(lines);
 			this.setStartAtIndex(startAtIndex);
 			this.setEndAtIndex(endAtIndex);
+			this.setEmployees(employees);
+			this.setErrorLines(errorLines);
+			this.setSuccessLines(successLines);
+
+			this.setThread(new Thread(this, name));
+			LOG.info("..... STARTING THREAD " + name);
+			thread.start();
+		}
+
+		public void setThread(Thread thread) {
+			this.thread = thread;
+		}
+
+		public Thread getThread() {
+			return this.thread;
+		}
+
+		private IEmployee getEmployeeFromLine(String line) {
+			LOG.info("..... THREAD " + name + " Start converting 1 line to 1 employee");
+			try {
+				IEmployee employee = null;
+				if (line.isEmpty()) {
+					throw new Exception("Cannot convert empty line to employee");
+				}
+				String[] toks = line.split("\\|");
+				if (toks.length != 5) {
+					throw new Exception("Employee must have exactly 5 fields");
+				}
+				Integer type = Integer.parseInt(toks[0]);
+				String employeeName = toks[1];
+				String startDate = toks[2];
+				Double baseSalary = Double.parseDouble(toks[3]);
+				Integer workingDays = Integer.parseInt(toks[4]);
+				if (!Validator.isDDMMYYYY(startDate)) {
+					throw new Exception("Invalid date format");
+				}
+				switch (type) {
+				case 1:
+					employee = new Developer(employeeName, startDate, baseSalary, workingDays);
+					break;
+				case 2:
+					employee = new Tester(employeeName, startDate, baseSalary, workingDays);
+					break;
+				case 3:
+					employee = new Manager(employeeName, startDate, baseSalary, workingDays);
+					break;
+				default:
+					throw new Exception("Invalid employee type");
+				}
+				return employee;
+			} catch (Exception e) {
+				LOG.error("THREAD " + name + "[getEmployeeFromLine]: " + e);
+				return null;
+			} finally {
+				LOG.info("***** THREAD " + name + " Done converting");
+			}
 		}
 
 		@Override
 		public void run() {
-			
+			IEmployee employee = null;
+			for (int i = this.getStartAtIndex(); i <= this.getEndAtIndex(); i++) {
+				employee = getEmployeeFromLine(lines.elementAt(i));
+				if (employee != null) {
+					employee = calcSalaryFromBasicInfo(employee);
+					this.addEmployee(employee);
+					this.addSuccessLine(lines.elementAt(i));
+				} else {
+					this.addErrorLine(lines.elementAt(i));
+				}
+			}
+		}
+
+		public void addEmployee(IEmployee employee) {
+			this.employees.add(employee);
+		}
+
+		public void addSuccessLine(String line) {
+			this.successLines.add(line);
+		}
+
+		public void addErrorLine(String line) {
+			this.errorLines.add(line);
 		}
 
 		public String getName() {
@@ -52,11 +136,11 @@ public class Application {
 			this.name = name;
 		}
 
-		public List<String> getLines() {
+		public Vector<String> getLines() {
 			return lines;
 		}
 
-		public void setLines(List<String> lines) {
+		public void setLines(Vector<String> lines) {
 			this.lines = lines;
 		}
 
@@ -74,6 +158,30 @@ public class Application {
 
 		public void setEndAtIndex(Integer endAtIndex) {
 			this.endAtIndex = endAtIndex;
+		}
+
+		public Vector<IEmployee> getEmployees() {
+			return employees;
+		}
+
+		public void setEmployees(Vector<IEmployee> employees) {
+			this.employees = employees;
+		}
+
+		public Vector<String> getErrorLines() {
+			return errorLines;
+		}
+
+		public void setErrorLines(Vector<String> errorLines) {
+			this.errorLines = errorLines;
+		}
+
+		public Vector<String> getSuccessLines() {
+			return successLines;
+		}
+
+		public void setSuccessLines(Vector<String> successLines) {
+			this.successLines = successLines;
 		}
 
 	}
@@ -112,47 +220,6 @@ public class Application {
 		return error;
 	}
 
-	private static IEmployee getEmployeeFromLine(String line) {
-		LOG.info("..... Start converting 1 line to 1 employee");
-		try {
-			IEmployee employee = null;
-			if (line.isEmpty()) {
-				throw new Exception("Cannot convert empty line to employee");
-			}
-			String[] toks = line.split("\\|");
-			if (toks.length != 5) {
-				throw new Exception("Employee must have exactly 5 fields");
-			}
-			Integer type = Integer.parseInt(toks[0]);
-			String employeeName = toks[1];
-			String startDate = toks[2];
-			Double baseSalary = Double.parseDouble(toks[3]);
-			Integer workingDays = Integer.parseInt(toks[4]);
-			if (!Validator.isDDMMYYYY(startDate)) {
-				throw new Exception("Invalid date format");
-			}
-			switch (type) {
-			case 1:
-				employee = new Developer(employeeName, startDate, baseSalary, workingDays);
-				break;
-			case 2:
-				employee = new Tester(employeeName, startDate, baseSalary, workingDays);
-				break;
-			case 3:
-				employee = new Manager(employeeName, startDate, baseSalary, workingDays);
-				break;
-			default:
-				throw new Exception("Invalid employee type");
-			}
-			return employee;
-		} catch (Exception e) {
-			LOG.error("[getEmployeeFromLine]: " + e);
-			return null;
-		} finally {
-			LOG.info("***** Done converting");
-		}
-	}
-
 	private static Vector<String> getLinesFromFile(File input) throws IOException {
 		Vector<String> lines = new Vector<String>();
 		int data;
@@ -163,7 +230,6 @@ public class Application {
 			while ((data = iStream.read()) != -1) {
 				temp = (char) data;
 				if (temp == '\r' || temp == '\n') {
-
 					if (line.isEmpty()) {
 						continue;
 					}
@@ -247,15 +313,14 @@ public class Application {
 			LOG.info("------  DONE SAVING ERROR FILE ------");
 		}
 	}
-
+	
 	public static void main(String args[]) {
 
 		try {
 			File[] inboundFiles = getInboundDir().listFiles(new FileFilter());
+
 			LOG.info("___________ START PROCESSING FILES ___________");
 			for (File inboundFile : inboundFiles) {
-
-				IEmployee employee = null;
 				Vector<IEmployee> employees = new Vector<IEmployee>();
 				Vector<String> lines = null;
 				Vector<String> errorLines = new Vector<String>();
@@ -266,16 +331,11 @@ public class Application {
 				if (lines == null) {
 					continue;
 				}
-				for (String line : lines) {
-					employee = getEmployeeFromLine(line);
-					if (employee != null) {
-						employee = calcSalaryFromBasicInfo(employee);
-						employees.add(employee);
-						successLines.add(line);
-					} else {
-						errorLines.add(line);
-					}
-				}
+				LineSolver t1 = new LineSolver("ONE", lines, 0, lines.size() / 2, employees, errorLines, successLines);
+				LineSolver t2 = new LineSolver("TWO", lines, lines.size() / 2 + 1, lines.size() - 1, employees,
+						errorLines, successLines);
+				t1.getThread().join();
+				t2.getThread().join();
 				LOG.info("----->    DONE WITH INBOUND FILE    <-----");
 
 				if (!successLines.isEmpty()) {
@@ -285,9 +345,7 @@ public class Application {
 				if (!errorLines.isEmpty()) {
 					saveErrorFile(inboundFile.getName(), errorLines);
 				}
-
 			}
-
 		} catch (Exception e) {
 			LOG.error("[main]: " + e);
 		} finally {
